7d5c5beb806619d92d400758571f49608de3c4cc
Reuben Thomas <reubenthomas123@gmail.com>
2020-10-08 16:09:05 +0800

    standardised update rate, odom to state 2D conversion node

--- devcar_nav/nodes/localisation.py
+++ devcar_nav/nodes/localisation.py
@@ -12,10 +12,35 @@ class Localisation:
 
     def __init__(self):
 
-        # Wait and initialise service
+        rospy.init_node('odom_to_state2D')
+
+        # Initialize subscriber
         rospy.Subscriber("/camera/odom/sample", Odometry, self.pose_cb)
 
         # Initialise publishers
-        self.localisation_pub = rospy.Publisher('/ngeeann_av/state2D', State2D, queue_size=10)
+        self.localisation_pub = rospy.Publisher('/state2D', State2D, queue_size=10)
 
-        def pose_cb(self, msg):
+    def pose_cb(self, msg):
+        state = State2D()
+
+        # 2D Pose Information
+        state.pose.x = msg.pose.pose.position.x
+        state.pose.y = msg.pose.pose.position.y
+        state.pose.theta = 2.0 * np.arctan2(msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+        
+        # Aligning heading to y-axis, accounts for double rotation error
+        if state.pose.theta < 0.0:
+            state.pose.theta += 2.0 * np.pi
+
+        # 2D Twist Information
+        state.twist.x = msg.twist.twist.linear.x
+        state.twist.y = msg.twist.twist.linear.y
+        state.twist.w = msg.twist.twist.angular.z  
+
+        self.localisation_pub.publish(state)
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+	Localisation().run()       
