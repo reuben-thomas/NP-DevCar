cda921bfe1e295919933a3919f8f9fd67478b400
winstxnhdw <winstxnhdw@gmail.com>
2020-10-10 00:30:13 +0800

    good practice stuff

--- devcar_description/nodes/bad_and_old_dynamic_model.py
+++ devcar_description/nodes/bad_and_old_dynamic_model.py
@@ -1,43 +1,31 @@
 #!/usr/bin/env python
-
 import rospy
-import tf
 import numpy as np
+import tf
+import math
 
 from nav_msgs.msg import Path, Odometry
 from ackermann_msgs.msg import AckermannDrive
 
+# non-linear lateral bicycle model
 class NonLinearBicycleModel():
 
     def __init__(self, x=0.0, y=0.0, yaw=0.0, vx=0.000001, vy=0.0, omega=0.0):
+        
+        rospy.init_node('dynamic_model')
 
-        # Initialise Publishers
-        self.odom_pub = rospy.Publisher("/camera/odom/sample", Odometry, queue_size=30)	
-
-        # Initialise Subscribers
+		# Initialise Subscribers
         self.cmd_sub = rospy.Subscriber('/ackermann_cmd', AckermannDrive, self.cmd_cb)
+
+		# Initialise Publishers
+        self.odom_pub = rospy.Publisher("/camera/odom/sample", Odometry, queue_size=30)	
 
         # Publishes vehicle position to tf
         self.pose_broadcaster = tf.TransformBroadcaster()
 
-        # Load parameters
-        try:
-            self.model_params = rospy.get_param("/vehicle_model")
-            self.max_steer = self.model_params["steering_limits"]
-            self.L = self.model_params["wheelbase"]
-            self.Lf = self.model_params["centreofgravity_to_frontaxle"]
-            self.m = self.model_params["mass"]
-            self.Iz = self.model_params["moment_of_inertia_z"]
-            self.c_a = self.model_params["drag_coefficient"]
-            self.c_r1 = self.model_params["coefficient_of_kinetic_friction"]
+        self.rate = 10.0
+        self.dt = 0.1
 
-            self.tracker_params = rospy.get_param("/path_tracker")
-            self.frequency = self.tracker_params["update_frequency"]
-        
-        except:
-            raise Exception("Missing ROS parameters. Check the configuration file.")
-
-        # Class variables to use whenever within the class when necessary
         self.x = x
         self.y = y
         self.yaw = yaw
@@ -45,55 +33,58 @@ class NonLinearBicycleModel():
         self.vy = vy
         self.omega = omega
 
+        self.max_steer = 0.7  # [rad] max steering angle
+        self.L = 0.210  # [m] Wheel base of vehicle
+        self.Lr = self.L / 2.0  # [m]
+        self.Lf = self.L - self.Lr
+
+        # Properties of the NP DevCar
+        self.Iz = 0.3  # kg/m2
+        self.m = 3.0  # kg
+
+        # Cornering stiffness estimated calculation
+        # Reference: https://www.mchenrysoftware.com/medit32/readme/msmac/default.htm?turl=examplestirecorneringstiffnesscalculation1.htm
+        self.Cf = self.m * (self.Lr / self.L) * 0.5 * 0.165 * (180 / np.pi) # [N/rad]
+        self.Cr = self.m * (self.Lf / self.L) * 0.5 * 0.165 * (180 / np.pi) # [N/rad]
+
+        # Aerodynamic and friction coefficients
+        self.c_a = 1.36
+        self.c_r1 = 0.01
+
         self.throttle = 0.0
         self.delta = 0.0
 
-    def cmd_cb(self, msg):
 
+    def cmd_cb(self, msg):
         self.throttle = msg.speed
         self.delta = msg.steering_angle
 
     def update(self):
-
-        dt = 1 / self.frequency
-        Lr = self.L - self.Lf
-
-        '''
-        Cornering stiffness estimated calculation [N/rad]
-        Reference: https://www.mchenrysoftware.com/medit32/readme/msmac/default.htm?turl=examplestirecorneringstiffnesscalculation1.htm
-        '''
-        Cf = self.m * (Lr / self.L) * 0.5 * 0.165 * (180 / np.pi)
-        Cr = self.m * (self.Lf / self.L) * 0.5 * 0.165 * (180 / np.pi)
-
         self.delta = np.clip(self.delta, -self.max_steer, self.max_steer)
-        self.x = self.x + self.vx * np.cos(self.yaw) * dt - self.vy * np.sin(self.yaw) * dt
-        self.y = self.y + self.vx * np.sin(self.yaw) * dt + self.vy * np.cos(self.yaw) * dt
-        self.yaw = self.yaw + self.omega * dt
-        self.yaw = self.normalise_angle(self.yaw)
+        self.x = self.x + self.vx * math.cos(self.yaw) * self.dt - self.vy * math.sin(self.yaw) * self.dt
+        self.y = self.y + self.vx * math.sin(self.yaw) * self.dt + self.vy * math.cos(self.yaw) * self.dt
+        self.yaw = self.yaw + self.omega * self.dt
+        self.yaw = self.normalize_angle(self.yaw)
         
-        af = np.arctan2(self.vy + self.Lf * self.omega / self.vx - self.delta, 1.0)
-        ar = np.arctan2(self.vy - Lr * self.omega / self.vx, 1.0)
+        af = math.atan2(((self.vy + self.Lf * self.omega) / self.vx - self.delta), 1.0)
+        ar = math.atan2((self.vy - self.Lr * self.omega) / self.vx, 1.0)
 
-        Ffy = -Cf * af
-        Fry = -Cr * ar
-
+        Ffy = -self.Cf * af
+        Fry = -self.Cr * ar
         R_x = self.c_r1 * self.vx
         F_aero = self.c_a * self.vx ** 2
         F_load = F_aero + R_x
-
-        self.vx = self.vx + (self.throttle - Ffy * np.sin(self.delta) / self.m - F_load/self.m + self.vy * self.omega) * dt
-        self.vy = self.vy + (Fry / self.m + Ffy * np.cos(self.delta) / self.m - self.vx * self.omega) * dt
-        self.omega = self.omega + (Ffy * self.Lf * np.cos(self.delta) - Fry * Lr) / self.Iz * dt
+        self.vx = self.vx + (self.throttle - Ffy * math.sin(self.delta) / self.m - F_load/self.m + self.vy * self.omega) * self.dt
+        self.vy = self.vy + (Fry / self.m + Ffy * math.cos(self.delta) / self.m - self.vx * self.omega) * self.dt
+        self.omega = self.omega + (Ffy * self.Lf * math.cos(self.delta) - Fry * self.Lr) / self.Iz * self.dt
 
     def pub_odom(self):
-
         odom = Odometry()
 
         # Creating pose message
         odom.pose.pose.position.x = self.x
         odom.pose.pose.position.y = self.y
         odom.pose.pose.position.z = 0.0
-
         quaternion = tf.transformations.quaternion_from_euler(0, 0, self.yaw)
         odom.pose.pose.orientation.x = quaternion[0]
         odom.pose.pose.orientation.y = quaternion[1]
@@ -114,11 +105,11 @@ class NonLinearBicycleModel():
         self.odom_pub.publish(odom)
 
     def pub_tf(self):
-
+        # Publish transform
         quaternion = tf.transformations.quaternion_from_euler(0, 0, self.yaw)
         self.pose_broadcaster.sendTransform((self.x, self.y, 0.0), quaternion, rospy.Time.now(), "camera_link", "map")
 
-    def normalise_angle(self, angle):
+    def normalize_angle(self, angle):
         """
         Normalize an angle to [-pi, pi].
         :param angle: (float)
@@ -134,28 +125,22 @@ class NonLinearBicycleModel():
 
 def main():
 
-    # Initialise the node
-    rospy.init_node('dynamic_model')
-
     # Initialise the class
     bicycle_model = NonLinearBicycleModel()
     print("Initialized vehicle simulation with dynamic bicycle model")
 
     # Set update rate
-    r = rospy.Rate(bicycle_model.frequency)
+    r = rospy.Rate(bicycle_model.rate)
     
     while not rospy.is_shutdown():
         try:
             r.sleep()
-
             try:
                 bicycle_model.update()
                 bicycle_model.pub_odom()
                 bicycle_model.pub_tf()
-
             except:
                 pass
-
         except KeyboardInterrupt:
             print("Shutting down ROS node...")
 
