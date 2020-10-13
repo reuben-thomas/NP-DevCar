aea2da255e542fc506ae71b7c900c120ad7a3df1
winstxnhdw <winstxnhdw@gmail.com>
2020-10-14 04:23:50 +0800

    good practice stuff

--- devcar_nav/nodes/command_goals.py
+++ devcar_nav/nodes/command_goals.py
@@ -12,39 +12,43 @@ from geometry_msgs.msg import Point, Pose2D, PoseStamped
 from visualization_msgs.msg import Marker
 from utils.cubic_spline_planner import *
 
-class Planner(object):
+class Planner:
 	
 	def __init__(self):
 
 		self.marker = Marker()
 		self.goals = 0
 
-		rospy.init_node('rviz_goals')
+		# Initialise Publishers
+		self.goals_viz_pub = rospy.Publisher('/viz_goals', Marker, queue_size=10)
+		self.path_viz_pub = rospy.Publisher('/viz_path', Path, queue_size=10)
+		self.target_path_pub = rospy.Publisher('/target_path', Path2D, queue_size=10)
 
 		# Initialise Subscribers
 		rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.clicked_goals_cb)
-		#rospy.Subscriber("/camera/odom/sample", Odometry, self.pose_cb)		
+		# rospy.Subscriber("/camera/odom/sample", Odometry, self.pose_cb)		
 
-		# Initialise Publishers
-		self.goals_viz_pub = rospy.Publisher('/viz_goals', Marker, queue_size = 10)
-		self.path_viz_pub = rospy.Publisher('/viz_path', Path, queue_size=10)
-		self.target_path_pub = rospy.Publisher('/target_path', Path2D, queue_size=10)
+		# Load parameters
+        try:
+            self.planner_params = rospy.get_param("/vehicle_model")
+            self.ds = self.planner_params("distance_step")
+			self.frame_id = self.planner_params("frame_id")
+
+        except:
+            raise Exception("Missing ROS parameters. Check the configuration file.")
 
 		# Total Goals
 		self.ax = []
 		self.ay = []
 		
-		# CUrrent Goals
+		# Current Goals
 		self.cx = []
 		self.cy = []
 
-		self.frame_id = "map"
-
-		self.ds = 0.025
-
-
-	# fetch clicked way points
 	def clicked_goals_cb(self, msg):
+		'''
+		Fetch clicked way points
+		'''
 		self.goals += 1
 		# Adding to master 2D waypoint list
 		self.ax.append(msg.pose.position.x)
@@ -55,11 +59,12 @@ class Planner(object):
 		if (self.goals >= 2):
 			self.create_display_path()
 
-	# display way points on the map
 	def display_waypoint(self,x,y):
-
+		'''
+		Display way points on the map
+		'''
 		points = Marker()		
-		points.header.frame_id = "/map"	# publish path in map frame		
+		points.header.frame_id = "/map"	# Publish path in map frame		
 		points.type = points.POINTS
 		points.action = points.ADD
 		points.lifetime = rospy.Duration(0)
@@ -76,14 +81,15 @@ class Planner(object):
 		point.x = x
 		point.y = y
 		
-		points.points.append(point);
+		points.points.append(point)
+
 		# Publish the MarkerArray
 		self.goals_viz_pub.publish(points)
 
-
-	# creates cubic spline polynomial path displays to map
 	def create_display_path(self):
-
+		'''
+		Creates cubic spline polynomial path displays to map
+		'''
 		cx, cy, cyaw, a, b = calc_spline_course(self.ax, self.ay, self.ds)
 
 		target_path = Path2D()
@@ -115,9 +121,23 @@ class Planner(object):
 		self.target_path_pub.publish(target_path)
 		self.path_viz_pub.publish(viz_path)
 
-	def run(self):
-		rospy.spin()
+def main():
+
+	# Initialise the node
+    rospy.init_node('rviz_goals')
+
+    # Initialise the class
+    planner = Planner()
+
+	print("*********** waypoint.py: read and display way point on the map ***********")
+
+    while not rospy.is_shutdown():
+        try:
+            rospy.spin()
+
+        except KeyboardInterrupt:
+            print("Shutting down ROS node...")
+
 
 if __name__ == '__main__':
-	print "*********** waypoint.py: read and display way point on the map ***********"
-	Planner().run()
+	main()
