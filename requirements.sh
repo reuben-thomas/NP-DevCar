fd4d836154a4892a47fffa80bb7eb35944916aa4
Winston H <56998716+winstxnhdw@users.noreply.github.com>
2022-04-15 17:53:51 +0800

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -10,12 +10,12 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # apt-get installs
-sudo apt-get update
-sudo apt-get install git -y
-sudo apt-get install python-rospy -y
-sudo apt-get install python-pip -y
-sudo apt-get install ros-kinetic-rosserial -y
-sudo apt-get install ros-kinetic-rosserial-arduino -y
+sudo apt update
+sudo apt install git -y
+sudo apt install python-rospy -y
+sudo apt install python-pip -y
+sudo apt install ros-kinetic-rosserial -y
+sudo apt install ros-kinetic-rosserial-arduino -y
 
 # pip installs
 pip install --upgrade pip
